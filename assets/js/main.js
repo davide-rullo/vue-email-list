87885ae52c56d1f256e1571ff3890ad538ac45e1
davide-rullo <davide.rullo01@gmail.com>
2023-09-20 14:40:26 +0200

    add API call

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,21 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
-        };
+            randomEmails: ''
+        }
 
     },
+
+    mounted() {
+        console.log(axios);
+
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            console.log(response);
+            this.randomEmails = response.data.response;
+            
+        })
+    }
 
 }).mount('#app')
