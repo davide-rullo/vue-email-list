b900704c40ba816eeb77374a639174b0613dff84
davide-rullo <davide.rullo01@gmail.com>
2023-09-20 15:01:15 +0200

    add for loop
    added for loops to print randomEmails content

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            randomEmails: ''
+            randomEmails: []
         }
 
     },
@@ -10,13 +10,15 @@ createApp({
     mounted() {
         console.log(axios);
 
-        axios
-        .get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => {
-            console.log(response);
-            this.randomEmails = response.data.response;
-            
-        })
+        for (let i = 0; i < 10; i++) {
+
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response => {
+                    console.log(response);
+                    this.randomEmails.push(response.data.response);
+                })
+        }
     }
 
 }).mount('#app')
